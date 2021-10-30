587c7ab0faf5532534c42121d87049417d175174
Chirag Rajpal <johncenadx1.cr@gmail.com>
2021-10-30 10:57:58 +0530

    java file

---
+++ Series.java
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+ 
+
+class gfg
+{
+     
+public : double sum(int n)
+{
+    double i, s = 0.0;
+    for (i = 1; i <= n; i++)
+    s = s + 1/i;
+    return s;
+}
+};
+ 
+
+int main()
+{
+    gfg g;
+    int n = 5;
+    cout << "Sum is " << g.sum(n);
+    return 0;
+}
+ 
